326601fba9b3d693b8c46ad1295c467ec95fff44
amaia <amalarrea@gmail.com>
2022-01-21 13:29:04 +0100

    proyecto2

--- src/app/pages/carrusel/carrusel.module.ts
+++ src/app/pages/carrusel/carrusel.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { SwiperModule } from 'swiper/angular';
 import { CarruselPageRoutingModule } from './carrusel-routing.module';
 import { CarruselPage } from './carrusel.page';
 
 @NgModule({
   imports: [
+    SwiperModule,
     CommonModule,
     FormsModule,
     IonicModule,
